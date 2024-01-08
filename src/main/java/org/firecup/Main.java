250df8bc5633169d8f13ffeebdcf11ee8142d1f8
christianfirecup <christianfirecup@aol.com>
2024-01-08 05:32:36 -0500

    Started Refactoring and Reformating(RAR)

--- src/main/java/org/firecup/Main.java
+++ src/main/java/org/firecup/Main.java
@@ -3,7 +3,7 @@ package org.firecup;
 public class Main {
     public static void main(String[] args) {
         Player player = new Player();
-        Board board = new Board(player);
+        Board board = new Board();
 
 
 
